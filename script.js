62053e170edb3deb4ed5e1f4196b2c3c2b6b0b63
Adi4999 <78524367+Adi4999@users.noreply.github.com>
2022-04-11 19:36:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+
+
+const hourHand= document.querySelector('[data-hour-hand]')
+const minuteHand= document.querySelector('[data-minute-hand]')
+const secondHand= document.querySelector('[data-second-hand]')
+
+
+
+
+const setClock=()=>{
+
+const currentDate= new Date();
+const secondsRatio= currentDate.getSeconds()/60
+const minutesRatio= (secondsRatio + currentDate.getMinutes())/60
+const hoursRatio= (minutesRatio + currentDate.getHours())/12
+
+setRoatation(secondHand,secondsRatio)
+setRoatation(minuteHand,minutesRatio)
+setRoatation(hourHand,hoursRatio)
+}
+
+
+
+function setRoatation(element,rotationRatio){
+
+ element.style.setProperty('--rotation',rotationRatio*360)
+}
+setInterval(setClock,1000)
